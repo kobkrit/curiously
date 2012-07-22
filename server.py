d8e6d5280802a75e0245b7cb865f551a0726eb4d
Kobkrit Viriyayudhakorn <kobkrit@gmail.com>
2012-07-22 14:45:10 -0700

    Finish extracting the question part

--- server.py
+++ server.py
@@ -9,26 +9,28 @@ render = web.template.render('templates/')
 
 class index:
     def matchTupleAC(self, A, C, tuples, words):
-        print A, C
+        toReturn = []
         for a,b,c in tuples:
             if (re.match(A,a) and re.match(C,c)):
                 for word in words:
                     if word[0] == b:
-                        return word
-        return None
+                        toReturn.append(word)
+        return toReturn
     
     def matchTupleAB(self, A, B, tuples, words):
-        print A, B
+        toReturn = []
         for a,b,c in tuples:
             if (re.match(A,a) and re.match(B,b)):
                 for word in words:
                     if word[0] == c:
-                        return word
-        return None
+                        toReturn.append(word)
+        return toReturn
     
     
     def matchPOS(self, pos, parseTree):
-        toReturn = []
+        all = []
+        lenMax = 0
+        textMax = ""
         list = [m.start() for m in re.finditer('(?=\('+pos+')', parseTree)]
         print list
         for start in list:
@@ -42,82 +44,42 @@ class index:
                 keep+=ch
                 if (level == 0):
                     break;
-            toReturn.append(keep)
-        return toReturn
+            all.append(keep)
+            
+        for item in all:
+            if lenMax<len(item):
+                lenMax = len(item)
+                textMax = item
+        return textMax
     
+    def extractWord(self, item):
+        return " ".join(re.findall("\([A-Z]{2,3} ([^\(\)]+)\)",item))
     
     def extractAnswer(self, parsed):
-        response = "<html>"
-        for sentence in parsed['sentences']:
-            #Log
-            response+=str(sentence['text'])
-            response+="<hr>"
-            response+=str(parsed)
-            response+="<hr>"
-            
+        for sentence in parsed['sentences']:            
             parseTree = sentence['parsetree']
             
+            QW = index.matchPOS(self, "W.*", parseTree)
+            Q = index.extractWord(self,QW)
             
-            QWs = index.matchPOS(self, "W.*", parseTree)
-            response+="<b>All QWs:</b> "+str(QWs)+"<br>"
-            QW = filter(lambda x: re.match("\([^\(\)]+\)",x), QWs)
-            response+="<b>Good QW:</b> "+str(QW)+"<br>"
             
-            #NounPhrase Extraction
-            NPs = index.matchPOS(self, "N.*", parseTree)
-            response+="<b>All NPs:</b> "+str(NPs)+"<br>"
-            NP = filter(lambda x: re.match("\([^\(\)]+\)",x), NPs)
-            response+="<b>Good NP:</b> "+str(NP)+"<br>"
+            NP = index.matchPOS(self, "NP", parseTree)
+            N = index.extractWord(self,NP)
+            
+            VBN = index.matchPOS(self, "VBN", parseTree)
+            V = index.extractWord(self,VBN)
+        return Q,N,V
     
-            #Verb Extraction
-            VBs = index.matchPOS(self, "V.*", parseTree)
-            response+="<b>All VBs:</b> "+str(VBs)+"<br>"
-            VB = filter(lambda x: re.match("\([^\(\)]+\)",x), VBs)
-            response+="<b>Good VB:</b> "+str(VB)+"<br>"
-            
-            response+="<hr><hr>"
-
-#===============================================================================
-#        #Question Word Extraction
-#        QuestionWord = None
-#        print parsed['sentences'][0]
-#        for a in parsed['sentences'][0]['words']:
-#            if (re.match('W.*', a[1]['PartOfSpeech'])):
-#                QuestionWord = a
-#        if (QuestionWord == None):
-#            response+="Please have at least one question word, e.g., What, When, Where, Which, Who, and How."
-#            return response
-#        
-# 
-#        MainVerb = index.matchTupleAC(self,'advmod', QuestionWord[0], parsed['sentences'][0]['tuples'], parsed['sentences'][0]['words'])
-#        if (MainVerb == None):
-#            response+="Please have at least one main verb, e.g., is, am, are, does, do, have, has, and etc."
-#            return response;
-#        response+="<b>MainVerb:</b> "+str(MainVerb[0])+"<br>"
-#     
-#        #MainTarget Extraction
-#        MainTarget = index.matchTupleAB(self,'nsubj.*', str(MainVerb[0]), parsed['sentences'][0]['tuples'], parsed['sentences'][0]['words'])
-#        if (MainTarget == None):
-#           response+="Please have at least one noun, e.g., thing, Paris, human, ballon."
-#           return response;
-#        response+="<b>MainTarget:</b> "+str(MainTarget[0])+"<br>"
-#        
-#        #AuxVerb Extraction
-#        AuxVerb = index.matchTupleAB(self,'.*mod', str(MainTarget[0]), parsed['sentences'][0]['tuples'], parsed['sentences'][0]['words'])
-#        if (AuxVerb!=None):
-#           response+="<b>AuxVerb:</b> "+str(AuxVerb[0])+"<br>"
-#        
-#    
-#===============================================================================
-        return response
     
     def GET(self):
         return render.index()
     
     def POST(self):
         data = web.input()
-        parsed = ast.literal_eval(gv.corenlp.parse(data["question"]))
-        return index.extractAnswer(self, parsed)
+        question = data["question"]
+        parsed = ast.literal_eval(gv.corenlp.parse(question))
+        Q,N,V = index.extractAnswer(self, parsed)
+        return "<html>"+question+"<br>"+str((Q,N,V))+"</html>"
     
 if __name__ == "__main__":
     app = web.application(urls, globals())
