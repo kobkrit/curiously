1f53227ad60460dc029cfb47dca607ef34abd58a
Kobkrit Viriyayudhakorn <kobkrit@gmail.com>
2012-07-22 12:49:56 -0700

    remove questionParser.py

--- questionParser.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-from corenlp import *
-corenlp = StanfordCoreNLP()  # wait a few minutes...
-
-question = "When is the independence day?"
-parsed = corenlp.parse(question)
-print parsed
